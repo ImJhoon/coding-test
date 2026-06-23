e556dbad27548afb1725cb60e5909cde1ef237ab
한재훈 <116868206+ImJhoon@users.noreply.github.com>
2026-06-23 23:12:24 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int sum = 0;
+        
+        // 10진수 n을 3진법으로 변환 후 String 으로 데이터 타입 변경
+        String ter = Integer.toString(n, 3);
+        
+        for (int i = 1; i <= ter.length(); i++) {
+            sum += Math.pow(3, ter.length() - i) * Integer.parseInt(String.valueOf(ter.charAt(ter.length() - i)));
+        }
+        
+        
+        return sum;
+    }
+}
