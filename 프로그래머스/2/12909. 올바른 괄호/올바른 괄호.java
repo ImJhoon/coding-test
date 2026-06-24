2e225c4e28cb3f0efdc7f827f6a1b0504d31848d
한재훈 <116868206+ImJhoon@users.noreply.github.com>
2026-06-24 15:06:47 +0900

    [level 2] Title: 올바른 괄호, Time: 3.07 ms, Memory: 57.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,8 +3,11 @@ class Solution {
         boolean answer = false;
         int count = 0;
         
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
+        // 문자열 배열로 변환 후 순회
+        for (char c : s.toCharArray()) {
+            // '(' 나오면 반드시 대응되는 ')' 나와서 count가 0이되어야함
+            // 순회 도중 count가 음수가 되면 이후에 나오는 ( 개수와 무관하게 false
+            if (c == '(') {
                 count++;
             } else {
                 count--;
